264c45c23a9b2b3d43aae0f74825b4646144279c
j-l-i <jerry.inm07@gmail.com>
2023-03-07 13:31:11 -0700

    update about and home components

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function About() {
     return  (
         <div className="pb-10 pt-20 h-screen" id="about">
-            <h2 className="text-center text-xl mb-5 underline text-white">A little bit about me</h2>
+            <h2 className="text-center text-6xl mb-5 text-white">A little bit about me</h2>
             <p className="text-center text-white">
                 Welcome and thanks for checking out my website.<br />  I'm a budding software engineer looking for new opportunities.<br />
                 This site is meant to be an overview of my skills, current projects, and a small introduction to me as a person.<br />
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -4,8 +4,14 @@ import { BsGithub, BsLinkedin } from 'react-icons/bs'
 function Home() {
     return  (
         <div className="flex flex-col pt-20 pb-6 h-screen" id="home">
-            <h2 className="text-4xl md:text-6xl mb-1 md:mb-3 font-bold ml-64 mt-64 text-white" id="home">Jerry Inman</h2>
-            <h2 className="text-base md:text-2xl mb-3 ml-64 font-medium text-white">Full Stack Software Engineer</h2>
+            <div className="w-[700px] absolute right-0 mt-64 mr-20 font-semibold text-4xl">
+                Welcome to my website.  This site is meant to be a brief look into my skills and me as a person.<br />
+                This is a work in progress.  Thanks for stopping by.
+            </div>
+            <div className=" w-[600px]">
+                <h2 className="text-4xl md:text-6xl mb-1 md:mb-3 font-bold ml-64 mt-64 text-white" id="home">Jerry Inman</h2>
+                <h2 className="text-base md:text-2xl mb-3 ml-64 font-medium text-white">Full Stack Software Engineer</h2>
+            </div>
             <div className="flex space-x-5 ml-64">
                 <a href="https://github.com/J-L-I" rel="noreferrer noopener" target="_blank" className="transition duration-200 hover:scale-125"><BsGithub size="2rem" color="blue"/></a>
                 <a href="https://www.linkedin.com/in/jerry-inman-7a62ab193/" rel="noreferrer noopener" target="_blank" className="transition duration-200 hover:scale-125"><BsLinkedin size="2rem" color="blue"/></a>
