e39cac12f4e0468c0509a4fe112641ba13fa75fb
j-l-i <jerry.inm07@gmail.com>
2023-03-08 10:02:55 -0700

    nvm

--- src/components/data/project_data.tsx
+++ src/components/data/project_data.tsx
@@ -1,19 +1,19 @@
 export default [
     {
         title: "portfolio website",
-        imgUrl: "Portfolio/src/assets/my_site_img.jpg",
+        imgUrl: "src/assets/my_site_img.jpg",
         stack: ["React", "HTML", "TailwindCSS", "JavaScript", "TypeScript"],
         linkUrl: "http://localhost:5173/"
     },
     {
         title: "CSGO Stat Display",
-        imgUrl: "Portfolio/src/assets/cs_display.jpg",
+        imgUrl: "src/assets/cs_display.jpg",
         stack: [],
         linkUrl: "http://localhost:5173/"
     },
     {
         title: "GPU Aggregator",
-        imgUrl: "Portfolio/src/assets/gpu_display.jpg",
+        imgUrl: "src/assets/gpu_display.jpg",
         stack: [],
         linkUrl: "http://localhost:5173/"
     }
