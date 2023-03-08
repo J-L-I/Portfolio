49dbf2af8c8892162d299341f2bffdceb8310853
j-l-i <jerry.inm07@gmail.com>
2023-03-08 09:20:38 -0700

    Make experience section more readable

--- src/components/resumeItem.tsx
+++ src/components/resumeItem.tsx
@@ -6,13 +6,13 @@ function ResumeItem(job: {year: string, title: string, description: string}): JS
             <li className="mb-9 ml-3">
                 <div className="w-3 h-3 rounded-full mt-1.5" />
                 <p className="flex flex-wrap gap-4 flex-row items-center justify-center text-sm md:text-sm">
-                    <span className="inline-block px-2 py-2 font-bold rounded-md text-white">
+                    <span className="inline-block px-2 py-2 font-bold rounded-md text-white text-2xl">
                         {job.year}
                     </span>
-                    <span className="text-md font-bold text-white">
+                    <span className="text-2xl font-bold text-white">
                         {job.title}
                     </span>
-                    <span className="my-1 text-md font-semibold leading-non text-white">
+                    <span className="my-1 text-md text-2xl font-semibold leading-non text-white">
                         {job.description}
                     </span>
                 </p>
