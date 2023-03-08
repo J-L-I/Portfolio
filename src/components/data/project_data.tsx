2bd89e7558ded42c002850917a12bdbb7bcdbeff
j-l-i <jerry.inm07@gmail.com>
2023-03-08 16:06:29 -0700

    img test 4

--- src/components/data/project_data.tsx
+++ src/components/data/project_data.tsx
@@ -1,19 +1,19 @@
 export default [
     {
         title: "portfolio website",
-        imgUrl: "src/assets/my-site-img.jpg",
+        imgUrl: "/assets/my-site-img.jpg",
         stack: ["React", "HTML", "TailwindCSS", "JavaScript", "TypeScript"],
         linkUrl: "http://localhost:5173/"
     },
     {
         title: "CSGO Stat Display",
-        imgUrl: "src/assets/cs-display.jpg",
+        imgUrl: "/assets/cs-display.jpg",
         stack: [],
         linkUrl: "http://localhost:5173/"
     },
     {
         title: "GPU Aggregator",
-        imgUrl: "src/assets/gpu-display.jpg",
+        imgUrl: "/assets/gpu-display.jpg",
         stack: [],
         linkUrl: "http://localhost:5173/"
     }
