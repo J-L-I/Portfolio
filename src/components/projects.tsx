3e25dca3c9a894efbde91b741db11982788a7ee4
j-l-i <jerry.inm07@gmail.com>
2023-03-11 21:15:12 -0700

    current projects

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -6,7 +6,7 @@ import ProjectItem from './projectItem'
 function Projects() {
     return  (
         <div>
-            <h1 className="flex justify-center mt-20 pt-20 font-bold text-white" id="projects">Projects</h1>
+            <h1 className="flex justify-center mt-20 pt-20 font-bold text-white" id="projects">Current Projects</h1>
             <h2 className="flex justify-center text-center text-white">Current projects I'm working on.  Most of these projects are just for learning and fun.<br />  Click the image to visit the repo.</h2>
             <div className="flex flex-col md:flex-row items-center justify-center mt-1 pb-10 h-screen">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
